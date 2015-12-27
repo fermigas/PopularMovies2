f7d2e94474501a39dcf6c5ce5025a7811be5148e
jon <newbluetwo@gmail.com>
2015-12-27 10:40:37 -0500

    extracted movie object to to field

--- app/src/main/java/com/example/android/popularmovies/app/MovieDetailsFragment.java
+++ app/src/main/java/com/example/android/popularmovies/app/MovieDetailsFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,61 +31,71 @@ import com.squareup.picasso.Picasso;
 
 public class MovieDetailsFragment extends Fragment {
 
+    private final String LOG_TAG = MovieDetailsFragment.class.getSimpleName();
+
     private String mMovieStr;
+    private Movie movie;
 
     public MovieDetailsFragment() {
         setHasOptionsMenu(true);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.details_fragment, container, false);
-        Intent intent = getActivity().getIntent();
 
-        showAllMovieData(rootView, intent);
+        getMovieFromParcelableExtra();
+        showAllMovieData(rootView);
 
         return rootView;
     }
 
-    private void showAllMovieData(View rootView, Intent intent) {
-        if (intent != null && intent.hasExtra("movie")) {
-            Movie movie = intent.getParcelableExtra("movie");
+    private void getMovieFromParcelableExtra() {
 
-            showMovieDetails(rootView, movie);
+        Intent intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra("movie"))
+            movie = intent.getParcelableExtra("movie");
 
-        }
     }
 
-    private void showMovieDetails(View rootView, Movie movie) {
-        showMoviePoster(rootView, movie);
-        showMovieTitle(rootView, movie);
-        showScrollingMovieOverview(rootView, movie);
-        showMovieReleaseDate(rootView, movie);
-        showMovieVoteAverage(rootView, movie);
+    private void showAllMovieData(View rootView) {
+
+        if (movie != null )
+            showMovieDetails(rootView);
+
     }
 
-    private void showMovieVoteAverage(View rootView, Movie movie) {
+    private void showMovieDetails(View rootView) {
+        showMoviePoster(rootView);
+        showMovieTitle(rootView);
+        showScrollingMovieOverview(rootView);
+        showMovieReleaseDate(rootView);
+        showMovieVoteAverage(rootView);
+    }
+
+    private void showMovieVoteAverage(View rootView) {
         ((TextView) rootView.findViewById(R.id.details_rating))
                 .setText(getString(R.string.movie_details_vote_average) + movie.voteAverage + "/10");
     }
 
-    private void showMovieReleaseDate(View rootView, Movie movie) {
+    private void showMovieReleaseDate(View rootView) {
         ((TextView) rootView.findViewById(R.id.details_release_date))
                 .setText(getString(R.string.movie_details_release_date) + movie.releaseDate);
     }
 
-    private void showScrollingMovieOverview(View rootView, Movie movie) {
+    private void showScrollingMovieOverview(View rootView) {
         TextView tv = ((TextView) rootView.findViewById(R.id.details_overview));
         tv.setMovementMethod(new ScrollingMovementMethod());
         tv.setText(movie.overview);
     }
 
-    private void showMovieTitle(View rootView, Movie movie) {
+    private void showMovieTitle(View rootView) {
         ((TextView) rootView.findViewById(R.id.details_title)).setText(movie.title);
     }
 
-    private void showMoviePoster(View rootView, Movie movie) {
+    private void showMoviePoster(View rootView) {
         String fullPosterPath =
                 getContext().getString(R.string.tmdb_base_image_url) +
                         getContext().getString(R.string.tmdb_image_size_342) +
